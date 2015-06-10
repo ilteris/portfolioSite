3546c3f7e15819f07d8223e11dbf74848deaf980
ilteris <ilteris@gmail.com>
2015-06-10 00:01:40 -0400

    working liquid

--- app/routes/works.js
+++ app/routes/works.js
@@ -17,7 +17,7 @@ var Work = Ember.Object.extend({
 });
 
 var poetics = Work.create({
-  name: 'Poetics',
+  name: 'POETICS',
   year: '2013',
   description:'Lorem Ipsum Poetics',
   image:'assets/images/poetics_iphone.png',
@@ -32,6 +32,89 @@ var tse = Work.create({
   logo:'assets/images/tse_logo.png'
 });
 
+var minibook = Work.create({
+  name: 'MINIBOOK',
+  year: '2014',
+  description:'MiniBook is an iphone app that explores storytelling in its own format. The format',
+  image:'assets/images/minibook_iphone.png',
+  logo:'assets/images/minibook_logo.png'
+});
+
+var wemontage = Work.create({
+  name: 'WEMONTAGE',
+  year: '2013',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/wemontage_logo.png',
+  logo:'assets/images/wemontage_mac.png'
+});
+
+
+var sferra = Work.create({
+  name: 'SFERRA',
+  year: '2012',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/sferra_iphone.png',
+  logo:'assets/images/sferra_logo.png'
+});
+
+
+
+var smc = Work.create({
+  name: 'SEEING MORE COLORS',
+  year: '2012',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/smc_iphone.png',
+  logo:'assets/images/SEEING_logo.png'
+});
+
+
+var oneworld = Work.create({
+  name: 'ONEWORLD',
+  year: '2011',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/oneworld_ipad.png',
+  logo:'assets/images/%7Boneworld%7D_logo.png'
+});
+
+
+var redfish = Work.create({
+  name: 'SEEING MORE COLORS',
+  year: '2010',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/redfish_ipad.png',
+  logo:'assets/images/RedFish_logo.png'
+});
+
+
+var moet = Work.create({
+  name: 'MOET BUBBLES',
+  year: '2008',
+  description:'Lorem Ipsum Moet',
+  image:'assets/images/moet_mac.png',
+  logo:'assets/images/moet1280_logo.png'
+});
+
+
+var remy = Work.create({
+  name: 'REMY MARTIN GAME NIGHTS',
+  year: '2007',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/remy_mac.png',
+  logo:'assets/images/remymartin_logo.png'
+});
+
+
+var whotels = Work.create({
+  name: 'W HOTELS HOBOKEN',
+  year: '2006',
+  description:'Lorem Ipsum TSE',
+  image:'assets/images/w_mac.png',
+  logo:'assets/images/whotels_logo.png'
+});
+
+
+
+
 
 
 var WorksCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -43,7 +126,7 @@ var WorksCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
 
 var works = WorksCollection.create();
 
-works.pushObjects([poetics, tse]);
+works.pushObjects([poetics, tse, minibook, sferra, oneworld, smc, redfish, remy, moet, whotels]);
 
 
 export default Ember.Route.extend({
--- app/transitions.js
+++ app/transitions.js
@@ -1,11 +1,24 @@
 /**
  * Created by ilteris on 6/2/15.
  */
-export default function(){
+export default function() {
+
+  var duration = 1500;
+
   this.transition(
-    this.fromRoute('main'),
-    this.toRoute('tut2'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.fromRoute('works'),
+    this.toRoute('work'),
+    this.use('toLeft', {
+      matchBy: 'data-speaker-id',
+      //use: ['webFlyTo', { duration } ]
+    }, {
+     // use: ['toLeft', { duration } ]
+    }),
+    this.reverse('toRight', {
+      matchBy: 'data-speaker-id',
+    //  use: ['webFlyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
   );
 }
