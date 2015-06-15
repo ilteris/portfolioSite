62e7fe49de6e5a2109a1df0f7ee51ee36d8d9323
ilteris <ilteris@gmail.com>
2015-06-15 10:39:32 -0400

    s

--- app/controllers/works.js
+++ app/controllers/works.js
@@ -3,9 +3,19 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   firstElement: function () {
-    return this.get('model.firstObject'); // or this.get('model.firstObject');
-  }.property('model.[]')
+    return this.get('model.firstObject');
+  }.property('model.[]'),
 
+  selectedElement: null,
 
+  currentElement: function () {
+    return (this.get('selectedElement') || this.get('firstElement'));
+  }.property('firstElement', 'selectedElement'),
 
+  actions: {
+    loadRight: function (selection) {
+      this.set('selectedElement', selection);
+      return false; // or something transition logic
+    }
+  }
 });
